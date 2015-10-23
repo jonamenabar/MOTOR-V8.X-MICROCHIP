6a944d2fa1b4d5066b264164573caed3d8a265b7
jonamenabar <jonamenabar@hotmail.com>
2015-10-22 21:53:41 -0300

    Funciona Pero aun persiste un problema solo con el PORTA
    se soluciono que no se alteren los otros valores de los cilindros, (se
    implemento una or) funciona en todos los puertos, ECEPTO en el PORTA
    (cilindro 1 y 3), se agrego la función apagar al comienzo. REVISAR EL
    PROBLEMA DEL PUERTO A, Y REVISAR LA FUNCION APAGAR (si altera o no al
    PORTB para lectura.

--- Main.c
+++ Main.c
@@ -36,34 +36,37 @@
 
 //-------------------DEFINO LEDS------------------
 //Cilindro 1
-#define ROJO1  PORTAbits.RA0=1
-#define CYAN1  PORTA=0b000110//PORTAbits.RA1=1 , PORTAbits.RA2=1  //VERDE BIT 4 PUERTO1  (BIT4 Y BIT 2 ON) VERDE+AZUL
-#define AZUL1	PORTAbits.RA2=1
-#define AMARILLO1 PORTA=0b000011//PORTAbits.RA1=1 , PORTAbits.RA0 // VERDE BIT 4 + ROJO BIT 3
-#define BLANCO1  PORTA=0b000111//PORTAbits.RA0=1 , PORTAbits.RA1=1 , PORTAbits.RA2=1 // BIT 3+ BIT 2+BIT 4
+#define ROJO1  PORTA|=0b000001 //RA0 (HAGO UN OR PARA NO CAMBIAR LOS VALORES ANTES DEFINIDOS
+#define CYAN1  PORTA|= 0b000110//PORTAbits.RA1=1 , PORTAbits.RA2=1  //VERDE BIT 4 PUERTO1  (BIT4 Y BIT 2 ON) VERDE+AZUL
+#define AZUL1	PORTA|= 0b000100 //RA2
+#define AMARILLO1 PORTA|= 0b000011//PORTAbits.RA1=1 , PORTAbits.RA0 // VERDE BIT 4 + ROJO BIT 3
+#define BLANCO1  PORTA|=0b000111//PORTAbits.RA0=1 , PORTAbits.RA1=1 , PORTAbits.RA2=1 // BIT 3+ BIT 2+BIT 4
 
 //Cilindro 3
-#define ROJO3  PORTAbits.RA3=1
-#define CYAN3  PORTA=0b110000//PORTAbits.RA4=1 , PORTAbits.RA5=1 //BIT 2
-#define AZUL3	PORTAbits.RA5=1
-#define AMARILLO3  PORTA=0b011000//PORTAbits.RA4=1 , PORTAbits.RA3=1
-#define BLANCO3	 PORTA=0b111000//PORTAbits.RA3=1 , (PORTAbits.RA4=1 , PORTAbits.RA5=1)
+#define ROJO3  PORTA|=0b001000 //RA3
+#define CYAN3  PORTA|=0b110000//PORTAbits.RA4=1 , PORTAbits.RA5=1 //BIT 2
+#define AZUL3	PORTA|=0b100000 //RA5
+#define AMARILLO3  PORTA|=0b011000//PORTAbits.RA4=1 , PORTAbits.RA3=1
+#define BLANCO3	 PORTA|=0b111000//PORTAbits.RA3=1 , (PORTAbits.RA4=1 , PORTAbits.RA5=1)
 
 //cilindro 5
-#define ROJO5  PORTCbits.RC0=1
-#define CYAN5  PORTC=0b00000110//PORTCbits.RC1=1 , PORTCbits.RC2=1
-#define AZUL5	PORTCbits.RC2=1
-#define AMARILLO5 PORTC=0b00000011//PORTCbits.RC1=1 , PORTCbits.RC0=1
-#define BLANCO5 PORTC=0b00000111//PORTCbits.RC0=1 , (PORTCbits.RC1=1 , PORTCbits.RC2)
+#define ROJO5  PORTC|=0b00000001 //RC0
+#define CYAN5  PORTC|=0b00000110//PORTCbits.RC1=1 , PORTCbits.RC2=1
+#define AZUL5	PORTC|=0b00000100 //RC2
+#define AMARILLO5 PORTC|=0b00000011//PORTCbits.RC1=1 , PORTCbits.RC0=1
+#define BLANCO5 PORTC|=0b00000111//PORTCbits.RC0=1 , (PORTCbits.RC1=1 , PORTCbits.RC2)
 
 //cilindro 7
-#define ROJO7  PORTBbits.RB1=1
-#define CYAN7  PORTB=0b00010100//PORTBbits.RB2=1 , PORTBbits.RB4=1
-#define AZUL7	PORTBbits.RB4=1
-#define AMARILLO7 PORTB=0b00000110//PORTBbits.RB1=1 , PORTBbits.RB2=1
-#define BLANCO7 PORTB=0b00010110//PORTBbits.RB1=1 , (PORTBbits.RB2=1 , PORTBbits.RB4)
+#define ROJO7  PORTB|=0b00000010 //RB1
+#define CYAN7  PORTB|=0b00010100//PORTBbits.RB2=1 , PORTBbits.RB4=1
+#define AZUL7	PORTB|=0b00010000 //RB4
+#define AMARILLO7 PORTB|=0b00000110//PORTBbits.RB1=1 , PORTBbits.RB2=1
+#define BLANCO7 PORTB|=0b00010110//PORTBbits.RB1=1 , (PORTBbits.RB2=1 , PORTBbits.RB4)
 
 #define ANTIREBOTE __delay_ms(50)//ANTI REBOTE, REVISAR SI EL TIEMPO ES MUY LARGO
+#define DELAY_FLASH __delay_ms(10) //establezco un delay para los flash
+
+void Apagar(void); //prototipado de la funcion apagar
 
 void main(void) {
 
@@ -117,82 +120,95 @@ void main(void) {
 
         do {
             if (SENSOR_AR == SI) {
-                
+
                 inicio = SI;
                 //ANTIREBOTE;
             }
         } while (inicio == 0);
 
         if (SENSOR_AR == SI) {
-            
+
             contador = 0;
-           // ANTIREBOTE;
+            // ANTIREBOTE;
 
         }
 
         if (SENSOR_CI == SI) {
-           
+
             contador++;
             // ANTIREBOTE;
         }
         switch (contador) {
             case 0:
                 BLANCO1;
+                DELAY_FLASH;
                 AMARILLO1;
                 AZUL3;
                 ROJO5;
                 ROJO7;
+                Apagar();
                 break;
             case 1:
                 AMARILLO1;
                 CYAN3;
                 AZUL5;
                 ROJO7;
+                Apagar();
                 break;
             case 2:
                 ROJO1;
                 CYAN3;
                 AZUL5;
                 AZUL7;
+                Apagar();
                 break;
             case 3:
                 ROJO1;
                 BLANCO3;
+                DELAY_FLASH;
                 AMARILLO3;
                 CYAN5;
                 AZUL7;
+                Apagar();
                 break;
             case 4:
                 AZUL1;
                 AMARILLO3;
                 CYAN5;
                 CYAN7;
+                Apagar();
                 break;
             case 5:
                 AZUL1;
                 ROJO3;
                 BLANCO5;
+                DELAY_FLASH;
                 AMARILLO5;
                 CYAN7;
+                Apagar();
                 break;
             case 6:
                 CYAN1;
                 ROJO3;
                 AMARILLO5;
                 BLANCO7;
+                DELAY_FLASH;
                 AMARILLO7;
+                Apagar();
                 break;
             case 7:
                 CYAN1;
                 AZUL3;
                 ROJO5;
                 AMARILLO7;
+                Apagar();
                 break;
             case 8:
                 CYAN1;
                 AZUL3;
                 ROJO5;
                 AMARILLO7;
+                Apagar();
                 break;
             default:
                 break;
@@ -203,4 +219,11 @@ void main(void) {
 
 
     return;
+}
+
+void Apagar(void) {
+    PORTA = 0; //OJO REVISAR PORQUE NO SE QUE OCURRIRIA AL TRATAR DE ESCRIBIR UN PUERTO DE ENTRADA
+    PORTB = 0;
+    PORTC = 0;
+
 }
