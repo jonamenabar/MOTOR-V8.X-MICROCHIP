594cb2d685961b06589b9bbfd0b0fdcf9bdd9437
jonamenabar <jonamenabar@hotmail.com>
2016-09-16 12:50:34 -0300

    FUNCIONANDO Y SINCRONIZADO
    Se repararon algunas fallas comunes, se optimizo el código pero un NO
    se organiza. Se agrego el destello doble de la chispa.
    y se comprobó su funcionamiento comparándolo con un video que muestra
    el movimiento y estados de los cilindros.

--- Main.c
+++ Main.c
@@ -36,37 +36,37 @@
 
 //-------------------DEFINO LEDS------------------
 //Cilindro 1
-#define ROJO1  /*PORTA|=0b000001 //RA0 (HAGO UN OR PARA NO CAMBIAR LOS VALORES ANTES DEFINIDOS*/ PORTAbits.RA0=1;
-#define CYAN1  /*PORTA|= 0b000110*/PORTAbits.RA1=1 ; PORTAbits.RA2=1;  //VERDE BIT 4 PUERTO1  (BIT4 Y BIT 2 ON) VERDE+AZUL 
-#define AZUL1	/*PORTA|= 0b000100 //RA2*/ PORTAbits.RA2=1;
-#define AMARILLO1 /*PORTA|= 0b000011*/ PORTAbits.RA1=1 ; PORTAbits.RA0=1; // VERDE BIT 4 + ROJO BIT 3
-#define BLANCO1  PORTA|=0b000111//PORTAbits.RA0=1 ; PORTAbits.RA1=1 ; //PORTAbits.RA2=1 // BIT 3+ BIT 2+BIT 4
+#define ROJO1     RA0=1
+#define CYAN1     RA1=1 ; RA2=1  //VERDE BIT 4 PUERTO1  (BIT4 Y BIT 2 ON) VERDE+AZUL 
+#define AZUL1	  RA2=1
+#define AMARILLO1 RA1=1 ; RA0=1 // VERDE BIT 4 + ROJO BIT 3
+#define BLANCO1   RA0=1 ; RA1=1 ; RA2=1//PORTAbits.RA0=1 ; PORTAbits.RA1=1 ; //PORTAbits.RA2=1 // BIT 3+ BIT 2+BIT 4
 
 //Cilindro 3
-#define ROJO3  /*PORTA|=0b001000 //RA3*/ PORTAbits.RA3=1;
-#define CYAN3  /*PORTA|=0b110000/*/PORTAbits.RA4=1 ; PORTAbits.RA5=1; //BIT 2  VERDE+AZUL
-#define AZUL3	/*PORTA|=0b100000 //RA5*/ PORTAbits.RA5=1;
-#define AMARILLO3  /*PORTA|=0b011000//*/PORTAbits.RA4=1 ; PORTAbits.RA3=1; //VERDE + ROJO
-#define BLANCO3	 PORTA|=0b111000//PORTAbits.RA3=1 ; PORTAbits.RA4=1 ; PORTAbits.RA5=1;
+#define ROJO3     RA3=1
+#define CYAN3     RC3=1 ; RA5=1  // VERDE+AZUL
+#define AZUL3	  RA5=1
+#define AMARILLO3 RC3=1 ; RA3=1 //VERDE + ROJO
+#define BLANCO3	  RA3=1 ; RA5=1 ; RC3=1 //PORTAbits.RA3=1 ; PORTCbits.RC3=1 ; PORTAbits.RA5=1;
 
 //cilindro 5
-#define ROJO5  PORTC=0b00000001 //RC0*/ PORTCbits.RC0=1;
-#define CYAN5  PORTC=0b00000110//*/PORTCbits.RC1=1 ; PORTCbits.RC2=1;// VERDE+AZUL
-#define AZUL5	PORTC=0b00000100 //RC2*/ PORTCbits.RC2=1;
-#define AMARILLO5 PORTC=0b00000011//PORTCbits.RC1=1 ; PORTCbits.RC0=1;// VERDE + ROJO
-#define BLANCO5 PORTC=0b00000111//PORTCbits.RC0=1; PORTCbits.RC1=1 ; PORTCbits.RC2=1;
+#define ROJO5     RC0=1
+#define CYAN5     RC1=1 ; RC2=1// VERDE+AZUL
+#define AZUL5	  RC2=1
+#define AMARILLO5 RC1=1 ; RC0=1// VERDE + ROJO
+#define BLANCO5   RC0=1 ; RC1=1 ; RC2=1
 
 //cilindro 7
-#define ROJO7  PORTB=0b00000010 //RB1*/ PORTBbits.RB1=1;
-#define CYAN7  PORTB=0b00010100//*/PORTBbits.RB2=1 ; PORTBbits.RB4=1; // VERDE+AZUL
-#define AZUL7	PORTB=0b00010000 //RB4*/ PORTBbits.RB4=1;
-#define AMARILLO7 PORTB=0b00000110//*/PORTBbits.RB1=1 ; PORTBbits.RB2=1;// VERDE + ROJO
-#define BLANCO7 PORTB=0b00010110//*/PORTBbits.RB1=1 ; PORTBbits.RB2=1 ; PORTBbits.RB4=1;
+#define ROJO7     RB1=1
+#define CYAN7     RB2=1 ; RB4=1 // VERDE+AZUL
+#define AZUL7	  RB4=1
+#define AMARILLO7 RB1=1 ; RB2=1// VERDE + ROJO
+#define BLANCO7   RB1=1 ; RB2=1 ; RB4=1
 
 #define ANTIREBOTE  __delay_ms(500)  //ANTI REBOTE, REVISAR SI EL TIEMPO ES MUY LARGO
-#define DELAY_FLASH __delay_ms(100) //establezco un delay para los flash
+#define DELAY_FLASH __delay_ms(80) //establezco un delay para los flash
 #define RETARDO __delay_ms(4)    //ESTABLEZCO UN DELAY DE 100 ms
-#define TIEMPO_PRUEBA __delay_ms(2000); //2 segundos.
+#define TIEMPO_PRUEBA __delay_ms(2000); //10 segundos.
 
 
 void Apagar(void); //prototipado de la funcion apagar
@@ -93,25 +93,25 @@ void main(void) {
 
     //*********Configuracion de los pines**********
     //PINES DE ENTRADA DE DATOS
-    TRISBbits.TRISB0 = 1; //SENSOR ARBOL DE LEVA RB0
-    TRISBbits.TRISB5 = 1; //SENSOR CIGUEÑAL RB5
+    TRISB0 = 1; //SENSOR ARBOL DE LEVA RB0
+    TRISB5 = 1; //SENSOR CIGUEÑAL RB5
 
     //CONFIGURO LOS PINES DE SALIDA (PARA LUCES DE MOTOR) P1DIR |= BIT0;
-    TRISAbits.TRISA2 = 0; //CILINDRO 1 AZUL
-    TRISAbits.TRISA0 = 0; //CILINDRO 1 ROJO
-    TRISAbits.TRISA1 = 0; //CILINDRO 1 VERDE
+    TRISA2 = 0; //CILINDRO 1 AZUL
+    TRISA0 = 0; //CILINDRO 1 ROJO
+    TRISA1 = 0; //CILINDRO 1 VERDE
 
-    TRISAbits.TRISA5 = 0; //CILINDRO 3 AZUL
-    TRISAbits.TRISA3 = 0; //CILINDRO 3 ROJO
-    TRISAbits.TRISA4 = 0; //CILINDRO 3 VERDE
+    TRISA5 = 0; //CILINDRO 3 AZUL
+    TRISA3 = 0; //CILINDRO 3 ROJO
+    TRISC3 = 0; //CILINDRO 3 VERDE
 
-    TRISCbits.TRISC2 = 0; //CILINDRO 5 AZUL
-    TRISCbits.TRISC0 = 0; //CILINDRO 5 ROJO
-    TRISCbits.TRISC1 = 0; //CILINDRO 5 VERDE
+    TRISC2 = 0; //CILINDRO 5 AZUL
+    TRISC0 = 0; //CILINDRO 5 ROJO
+    TRISC1 = 0; //CILINDRO 5 VERDE
 
-    TRISBbits.TRISB1 = 0; //CILINDRO 7 ROJO
-    TRISBbits.TRISB2 = 0; //CILINDRO 7 VERDE
-    TRISBbits.TRISB4 = 0; //CILINDRO 7 AZUL
+    TRISB1 = 0; //CILINDRO 7 ROJO
+    TRISB2 = 0; //CILINDRO 7 VERDE
+    TRISB4 = 0; //CILINDRO 7 AZUL
 
     //************************************ secuencia de encendido de luces motor CILINDRO 1**************************************************************************************
     /*El cilindro 1 le corresponen los tiempos junto al contador como sigue>
@@ -133,127 +133,154 @@ void main(void) {
             }
         } while (inicio == 0);
 
-        if (SENSOR_AR == SI) {
+        if (SENSOR_AR == SI) { //Nota: Deberias meterlo en una interrupcion!.
 
-            contador = 0;
+            contador = 4; //ARRANCA EN EL PISTON 4
             ANTIREBOTE;
             Apagar();
-            UNAVEZ = 1;
+            
         }
 
-        if (SENSOR_CI == SI) {
+        if (SENSOR_CI == SI) { //Nota: Deberias meterlo en una interrupcion!.
 
             contador++;
+            UNAVEZ = 1;
             ANTIREBOTE;
             if(contador>=9)
                 contador=0;
         }
         switch (contador) {
+            
             case 0:
-                if (UNAVEZ == 1) {
-                    BLANCO1;
-                    DELAY_FLASH;
-                }
+                Apagar();
                 PORTA = 0b100011; //(AMARILLO1+AZUL3)
                 /*
                 AMARILLO1;
                 AZUL3;*/
-                
                 ROJO5;
                 ROJO7;
+                if (UNAVEZ == 1) { //ESTO ES PARA GENERAR UN DOBLE DESTELLO
+                    PORTA=0b100111;// BLANCO1, tiene el RA5 en 1 para no apagar el azul 3
+                    DELAY_FLASH;
+                    PORTA=0b100000;;// apago solamente BLANCO1
+                    DELAY_FLASH;
+                    PORTA=0b100111;// BLANCO1, tiene el RA5 en 1 para no apagar el azul 3
+                    DELAY_FLASH;
+                }
                 UNAVEZ = 0;
                 break;
+            
             case 1:
+                Apagar();
                 PORTA = 0b110011;
+                RC3=1;
                 /*AMARILLO1;
                 CYAN3;*/
                 
                 AZUL5;
                 ROJO7;
-
                 break;
+            
             case 2:
-                PORTA = 0b110001;
+                Apagar();
+                PORTA = 0b100001;
+                RC3=1;
                 /*ROJO1;
                 CYAN3;*/
                 AZUL5;
                 AZUL7;
-
                 break;
+            
             case 3:
-
-
-                if (UNAVEZ == 1) {
-                    BLANCO3;
-                    DELAY_FLASH;
-                }
-                PORTA = 0b011001;//rojo1+amarillo3)
+                Apagar();
+                PORTA = 0b001001;
+                RC3=1;//rojo1+amarillo3)
                 /*ROJO1;
                 AMARILLO3;*/
                 CYAN5;
                 AZUL7;
+                if (UNAVEZ == 1) {
+                    PORTA=0b101001; //BLANCO3 //ESTO ES PARA GENERAR UN DOBLE DESTELLO
+                    RC3=1;
+                    DELAY_FLASH;
+                    PORTA=0b000001; //apago solo BLANCO3, dejo prendido el rojo1
+                    RC3=0;
+                    DELAY_FLASH;
+                    PORTA=0b101001; //BLANCO3
+                    RC3=1;
+                    DELAY_FLASH;
+                }
                 UNAVEZ = 0;
-
                 break;
+            
             case 4:
-                PORTA = 0b011100; //(AZUL1+AMARILLO3)
+                Apagar();
+                PORTA = 0b001100;
+                RC3=1; //(AZUL1+AMARILLO3)
 
                 /*AZUL1;
                 AMARILLO3;
                  */
                 CYAN5;
                 CYAN7;
-
                 break;
+            
             case 5:
-
-
-                if (UNAVEZ == 1) {
-                    BLANCO5;
-                    DELAY_FLASH;
-                }
+                Apagar();
                 PORTA = 0b001100;
                 /*AZUL1;
                 ROJO3;*/
                 AMARILLO5;
                 CYAN7;
+                if (UNAVEZ == 1) {     //ESTO ES PARA GENERAR UN DOBLE DESTELLO
+                    BLANCO5;
+                    DELAY_FLASH;
+                    RC0=0;RC1=0;RC2=0; //APAGO EL BLANCO 5
+                    DELAY_FLASH;
+                    BLANCO5;
+                    DELAY_FLASH;
+                }
                 UNAVEZ = 0;
-
                 break;
+            
             case 6:
-
+                Apagar();
                 PORTA = 0b001110;
                 /*CYAN1;
                 ROJO3;*/
                 AMARILLO5;
-                if (UNAVEZ == 1) {
+                AMARILLO7;
+                if (UNAVEZ == 1) {     //ESTO ES PARA GENERAR UN DOBLE DESTELLO
                     BLANCO7;
                     DELAY_FLASH;
+                    RB0=0;RB2=0;RB4=0;
+                    DELAY_FLASH;
+                    BLANCO7;
+                    DELAY_FLASH;    
                 }
-                AMARILLO7;
                 UNAVEZ = 0;
-
                 break;
+            
             case 7:
-                PORTA |= 0b100110;
+                Apagar();
+                PORTA = 0b100110;
                 /*CYAN1;
                 AZUL3;*/
                 ROJO5;
                 AMARILLO7;
-                
-
                 break;
-            case 8:
-
-                PORTA |= 0b100110;
+            
+            case 8: //creo q no hay caso 8
+               /* Apagar();
+                PORTA = 0b100110;
                 /*CYAN1;
-                AZUL3;*/
+                AZUL3;
                 ROJO5;
-                AMARILLO7;
-                
-
+                AMARILLO7;*/
+                contador = 0;//revisar si es correcto
                 break;
             default:
+                contador = 4; //vuelvo a comenzar (es x si el contador tuvo un valor erroneo)
                 break;
         }
 
@@ -272,13 +299,9 @@ void Apagar(void) {
 }
 
 void prueba(void) {
-
-    PORTA|=0b000111;
-    TIEMPO_PRUEBA;
-    PORTA|=0b111111;
-    TIEMPO_PRUEBA;
-    BLANCO5;
-    TIEMPO_PRUEBA;
-    BLANCO7;
-    TIEMPO_PRUEBA;
+//pongo todos los leds en 1
+    PORTA=47;
+    PORTB=22;
+    PORTC=15;
+    //TIEMPO_PRUEBA;
 }
